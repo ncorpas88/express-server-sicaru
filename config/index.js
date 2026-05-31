33cde2fb895c936f6e3b7d35f36ddf3e7c87cba9
Natanael Corpas Rivero <natel1988@gmail.com>
2026-05-31 23:49:07 +0200

    commit 31/5

--- config/index.js
+++ config/index.js
@@ -1,32 +1,36 @@
 const express = require("express");
 
-// ℹ️ Logs incoming requests and responses to the terminal (useful for debugging)
+// LOGGER
 const logger = require("morgan");
 
-// ℹ️ Allows the server to accept requests from different origins (e.g., frontend apps)
-// CORS (Cross-Origin Resource Sharing) enables secure cross-origin requests.
+// CORS
 const cors = require("cors");
 
-// Middleware configuration
 function config(app) {
-  // ℹ️ Enables Express to trust reverse proxies (e.g., when deployed behind services like Heroku or Vercel)
+  // TRUST PROXY
   app.set("trust proxy", 1);
-  
-  // ℹ️ Configures CORS to allow requests only from the specified origin
+
+  // CORS
   app.use(
     cors({
-      origin: [process.env.ORIGIN]
-    })
+      origin: process.env.ORIGIN,
+
+      credentials: true,
+    }),
   );
-  
-  // ℹ️ Logs requests in the development environment
-  app.use(logger("dev")); 
 
-  // ℹ️ Parses incoming JSON requests
-  app.use(express.json()); 
+  // LOGGER
+  app.use(logger("dev"));
 
-  // ℹ️ Parses incoming request bodies with URL-encoded data (form submissions)
-  app.use(express.urlencoded({ extended: false }));
-};
+  // JSON
+  app.use(express.json());
 
-module.exports = config
+  // URL ENCODED
+  app.use(
+    express.urlencoded({
+      extended: false,
+    }),
+  );
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // ℹ️ Loads environment variables from a .env file into process.env
 try {
-  process.loadEnvFile()
-} catch(error) {
-  console.warn(".env file not found, using default environment values")
+  process.loadEnvFile();
+} catch (error) {
+  console.warn(".env file not found, using default environment values");
 }
 
 // ℹ️ Establishes a connection to the database
@@ -10,17 +10,17 @@ require("./db");
 
 // Imports Express (a Node.js framework for handling HTTP requests) and initializes the server
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5174",
-  })
-)
+    origin: "http://localhost:5173",
+  }),
+);
 
 // ℹ️ Loads and applies global middleware (CORS, JSON parsing, etc.) for server configurations
-const config = require("./config")
+const config = require("./config");
 config(app);
 
 // 👇 Defines and applies route handlers
@@ -28,7 +28,7 @@ const indexRouter = require("./routes/index.routes");
 app.use("/api", indexRouter);
 
 // ❗ Centralized error handling (must be placed after routes)
-const handleErrors = require("./errors")
+const handleErrors = require("./errors");
 handleErrors(app);
 
 // ℹ️ Defines the server port (default: 5005)
